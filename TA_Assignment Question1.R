66166bc0e29cf58674a59c81c16a243879c037b6
Swetha Pentam <45655658+Swetha-Pentam@users.noreply.github.com>
2018-12-23 23:49:09 +0530

    Add files via upload

---
+++ TA_Assignment Question1.R
@@ -0,0 +1,10 @@
+# Group Project  
+#Swetha Pentam: 11810068
+#Sudhavani Manne: 11810067
+
+rm(list=ls())
+library(shiny)
+
+source("https://raw.githubusercontent.com/Swetha-Pentam/TA_Assignment_Q1/master/dependancy.R")
+
+runGitHub("TA_Assignment_Q1","Swetha-Pentam")
